0dfe7ab0da20599ed3a95e21d1e6fb48f64cb60c
Carolin Walter <carolinwalter@gmx.net>
2024-06-18 19:50:27 +0200

    add JavaDoc comments to StorageroomAdapter.java

--- app/src/main/java/com/iu/storageroom/StorageroomAdapter.java
+++ app/src/main/java/com/iu/storageroom/StorageroomAdapter.java
@@ -23,23 +23,37 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
+/**
+ * Adapter for displaying Storage Room items in a RecyclerView.
+ */
 public class StorageroomAdapter extends RecyclerView.Adapter<StorageroomAdapter.ViewHolder> {
     private List<Storageroom> storageroomList;
     private Context context;
     private OnEditClickListener editClickListener;
     private OnDeleteClickListener deleteClickListener;
-    private int[] iconResIds;
 
-    // Interface for edit click listener
+    /**
+     * Interface for handling edit button clicks.
+     */
     public interface OnEditClickListener {
         void onEditClick(Storageroom storageroom);
     }
 
-    // Interface for delete click listener
+    /**
+     * Interface for handling delete button clicks.
+     */
     public interface OnDeleteClickListener {
         void onDeleteClick(Storageroom storageroom);
     }
 
+    /**
+     * Constructor for the StorageroomAdapter.
+     *
+     * @param context            The context in which the adapter is being used.
+     * @param storageroomList    The list of storagerooms to display.
+     * @param editClickListener  Listener for edit button clicks.
+     * @param deleteClickListener Listener for delete button clicks.
+     */
     public StorageroomAdapter(Context context, List<Storageroom> storageroomList,
                               OnEditClickListener editClickListener, OnDeleteClickListener deleteClickListener) {
         this.context = context;
@@ -48,6 +62,13 @@ public class StorageroomAdapter extends RecyclerView.Adapter<StorageroomAdapter.
         this.deleteClickListener = deleteClickListener;
     }
 
+    /**
+     * Creates a new ViewHolder instance.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds a View of the given view type.
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -55,12 +76,17 @@ public class StorageroomAdapter extends RecyclerView.Adapter<StorageroomAdapter.
         return new ViewHolder(view);
     }
 
+    /**
+     * Binds data to the ViewHolder.
+     *
+     * @param holder   The ViewHolder to bind data to.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Storageroom storageroom = storageroomList.get(position);
         holder.storageroomName.setText(storageroom.getName());
 
-        //int iconResId = Integer.parseInt(storageroom.getSelectedIcon());
         int iconResId = storageroom.getSelectedIconInt();
         holder.storageroomImage.setImageResource(iconResId);
 
@@ -79,17 +105,30 @@ public class StorageroomAdapter extends RecyclerView.Adapter<StorageroomAdapter.
         });
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items.
+     */
     @Override
     public int getItemCount() {
         return storageroomList.size();
     }
 
+    /**
+     * ViewHolder class for holding the view elements of each item in the RecyclerView.
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView storageroomName;
         public ImageView storageroomImage;
         public ImageView editButton;
         public ImageView deleteButton;
 
+        /**
+         * Constructor for the ViewHolder.
+         *
+         * @param itemView The View object that represents the RecyclerView item.
+         */
         public ViewHolder(View itemView) {
             super(itemView);
             storageroomName = itemView.findViewById(R.id.storageroomName);
