3077121119cdca746167dafcd040e623635bb23c
Eugen Loskan <mybabel@gmx.net>
2024-06-27 19:27:30 +0200

    Code revision / rollback to previous logic.

--- app/src/main/java/com/iu/storageroom/ProductAdapter.java
+++ app/src/main/java/com/iu/storageroom/ProductAdapter.java
@@ -4,29 +4,22 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.iu.storageroom.model.Product;
+
 import java.util.List;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
-
     private Context context;
     private List<Product> productList;
-    private OnItemClickListener listener;
 
-    public interface OnItemClickListener {
-        void onItemClick(Product product);
-        void onSaveClicked(Product product);
-        void onCancelClicked();
-    }
-
-    public ProductAdapter(Context context, List<Product> productList, OnItemClickListener listener) {
+    public ProductAdapter(Context context, List<Product> productList) {
         this.context = context;
         this.productList = productList;
-        this.listener = listener;
     }
 
     @NonNull
@@ -40,25 +33,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Product product = productList.get(position);
         holder.productName.setText(product.getName());
-        holder.productQuantity.setText(product.getQuantity());
-
-        holder.buttonSave.setOnClickListener(v -> {
-            if (listener != null) {
-                listener.onSaveClicked(product);
-            }
-        });
-
-        holder.buttonCancel.setOnClickListener(v -> {
-            if (listener != null) {
-                listener.onCancelClicked();
-            }
-        });
-
-        holder.itemView.setOnClickListener(v -> {
-            if (listener != null) {
-                listener.onItemClick(product);
-            }
-        });
+        holder.productQuantity.setText(String.valueOf(product.getQuantity()));
     }
 
     @Override
@@ -69,15 +44,12 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView productName;
         public TextView productQuantity;
-        public Button buttonSave;
-        public Button buttonCancel;
 
         public ViewHolder(View itemView) {
             super(itemView);
             productName = itemView.findViewById(R.id.productName);
             productQuantity = itemView.findViewById(R.id.productQuantity);
-            buttonSave = itemView.findViewById(R.id.buttonSave);
-            buttonCancel = itemView.findViewById(R.id.btnCancel);
         }
     }
+
 }
